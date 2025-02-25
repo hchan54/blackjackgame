70fbef0b91a8cc36a6817002cf5c082d24f75944
hchan54 <hchan54@wisc.edu>
2025-02-25 14:50:42 -0600

    Adding HW02 Templates

---
+++ homework/common/black_jack/timer_050_ms.c
@@ -0,0 +1,28 @@
+/**
+ * @file timer_050_ms.c
+ * @author ADD YOUR NAMES HERE
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-13
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+ #include "timer_050_ms.h"
+
+
+ #if defined(HW02) || defined (HW03) 
+/* 50mS Timer Handles*/
+cyhal_timer_t       Timer_MS_050_Obj; 
+cyhal_timer_cfg_t   Timer_MS_050_Cfg;
+
+ void handler_timer_050_ms(void *callback_arg, cyhal_timer_event_t event)
+ {
+ }
+
+/* Configure a general purpose timer to generate an interrupt every 50mS*/
+ void timer_050_ms_start(void)
+ {
+ }
+
+#endif
